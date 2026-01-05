f4037453d85a633564b3f44f5961d54e3c0c14c2
Softgen Dev <dev@softgen.ai>
2026-01-05 18:06:53 +0000

    Add google_analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Script from 'next/script'
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,7 +11,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider>
       <LanguageProvider>
         <CurrencyProvider>
-          <Component {...pageProps} />
+          {/* Google Analytics 4 */}
+<Script
+    id="google-analytics-4"
+    src="https://www.googletagmanager.com/gtag/js?id=G-RMFC9ZZL82"
+    strategy="afterInteractive"
+/>
+              <Script
+                  id="google-analytics-4-inline"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                      __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-RMFC9ZZL82');`
+                  }}
+              />
+{/* End Google Analytics 4 */}
+<Component {...pageProps} />
           <Toaster />
         </CurrencyProvider>
       </LanguageProvider>
